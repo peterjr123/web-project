50931d4cfd9b3cdb123e45a1278a70d9d678fd27
ShaderBinK <yubinien@gmail.com>
2022-05-30 01:23:57 +0900

    feat: option music complete

--- src/main_display/main_display.js
+++ src/main_display/main_display.js
@@ -10,6 +10,72 @@ $(document).ready(function () {
 		$("#option_page").css("display", "block");
 	});
 
+	var music1 = globalAudio.normalStage[0];
+	var music2 = globalAudio.normalStage[1];
+	var music3 = globalAudio.normalStage[2];
+	var music4 = globalAudio.normalStage[3];
+	$("#music1").click(function() {
+		music1.play();
+		music2.pause();
+		music3.pause();
+		music4.pause();
+	});
+	$("#music2").click(function() {
+		music2.play();
+		music1.pause();
+		music3.pause();
+		music4.pause();
+	});
+	$("#music3").click(function() {
+		music3.play();
+		music1.pause();
+		music2.pause();
+		music4.pause();
+	});
+	$("#music4").click(function() {
+		music4.play();
+		music1.pause();
+		music2.pause();
+		music3.pause();
+	});
+
+	var ex_music = globalAudio.bossBattleAudio;
+	$("#music_on").click(function() {
+		music1.pause();
+		music2.pause();
+		music3.pause();
+		music4.pause();
+		ex_music.currentTime = 30;
+		ex_music.play();
+	});
+	$("#music_off").click(function() {
+		music1.pause();
+		music2.pause();
+		music3.pause();
+		music4.pause();
+		ex_music.pause();
+	});
+
+	var ex_effect = globalAudio.clickAudio;
+	$("#effect_on").click(function() {
+		music1.pause();
+		music2.pause();
+		music3.pause();
+		music4.pause();
+		ex_music.pause();
+		ex_effect.currentTime = 1;
+		ex_effect.play();
+	});
+	$("#effect_off").click(function() {
+		music1.pause();
+		music2.pause();
+		music3.pause();
+		music4.pause();
+		ex_music.pause();
+		ex_effect.pause();
+	});
+
+
 	$("#htp_btn").click(function () {
 		$("#howtoplay_page").addClass("popup");
 		$("#howtoplay_page").css("display", "block");
